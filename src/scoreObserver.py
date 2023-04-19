bc94277a5630789c6860e36ce3dafc04b563f9cd
jaydel14 <70174605+jaydel14@users.noreply.github.com>
2023-04-19 12:32:17 -0600

    added some comments to observer

--- src/scoreObserver.py
+++ src/scoreObserver.py
@@ -1,4 +1,7 @@
 import os
+# used a little help from Geeks for Geeks and Stack overflow to help form the obeserver patern into python.
+#even used a little help from ChatGPT, to change some of our code from FNCD obserever from java into python then
+#tweeked that code to work with our flappy bird game.
 
 class Subject:
     def __init__(self):
